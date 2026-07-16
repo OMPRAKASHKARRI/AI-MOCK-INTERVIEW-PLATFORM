01477ba2029340a1da585591716d5d6b92aafe91
OMPRAKASHKARRI <karribujji04@gmail.com>
2026-07-16 09:53:56 +0530

    Changing Ai model

--- project/server/src/config/gemini.config.js
+++ project/server/src/config/gemini.config.js
@@ -1,23 +1,20 @@
-// config/groq.config.js
-
 import Groq from 'groq-sdk'
 
 const groq = new Groq({
   apiKey: process.env.GROQ_API_KEY,
 })
 
-export const generateContent = async prompt => {
-  const completion =
-    await groq.chat.completions.create({
-      messages: [
-        {
-          role: 'user',
-          content: prompt,
-        },
-      ],
-      model: 'llama-3.3-70b-versatile',
-      temperature: 0.7,
-    })
+export const generateContent = async (prompt) => {
+  const completion = await groq.chat.completions.create({
+    model: 'openai/gpt-oss-120b',
+    messages: [
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    temperature: 0.7,
+  })
 
   return completion.choices[0].message.content
 }
